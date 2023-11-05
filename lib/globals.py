379ed4965eaf4db70a2bb1618cdf4b2292c7a33f
BigFudge <valter.martin@hotmail.com>
2023-11-05 22:35:23 +0100

    add constants

---
+++ lib/globals.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN') 
+OPENAI_API_KEY = os.environ.get('OPENAI_API_TOKEN')
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
+GITHUB_REPO = "github.com/valterm/dilly-dalle"
