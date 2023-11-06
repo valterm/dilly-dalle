14e7f5f42d3c0d4f91fa905028eb2dc9db27257e
BigFudge <valter.martin@hotmail.com>
2023-11-06 23:39:16 +0100

    add png conversion

--- lib/dalle.py
+++ lib/dalle.py
@@ -1,5 +1,8 @@
 import logging
 from .globals import LOGLEVEL
+from PIL import Image
+from io import BytesIO
+
 logging.basicConfig(level=LOGLEVEL, format='%(asctime)s - %(levelname)s - %(message)s')
 
 import requests
@@ -10,6 +13,19 @@ class Dalle:
     def __init__(self, api_key):
         logging.debug('Entering __init__')
         openai.api_key = api_key
+
+    def convert_to_png(self, image):
+        logging.debug('Entering convert_to_png')
+        '''
+        Converts an image to png.
+        Returns the converted image.
+        '''
+        image_jpg = Image.open(BytesIO(image))
+        image_png = BytesIO()
+        image_jpg.save(image_png, format='PNG')
+        image_png.seek(0)
+
+        return(image_png)
 
     logging.debug('Entering: generate_image')
     def generate_image(self, prompt, num_images=1, size="256x256"):
@@ -28,8 +44,10 @@ class Dalle:
         return(image_url)
     
     logging.debug('Entering: generate_image_variation')
+
     def generate_image_variation(self, image):
         logging.debug('Entering generate_image_variation')
+        
         '''
         Generates an image variation from an image.
         Returns the generated image's url.
@@ -42,3 +60,4 @@ class Dalle:
         image_url = r['data'][0]['url']
         logging.debug(f"Image URL: {image_url}")
         return(image_url)
+    
